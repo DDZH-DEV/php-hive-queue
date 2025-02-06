011b7608a115a8c841f6c443e427a9985eed6e00
DDZH <9rax.dev@gmail.com>
2025-02-06 17:21:12 +0800

    count

--- src/NoItemAvailableException.php
+++ src/NoItemAvailableException.php
@@ -13,7 +13,7 @@ namespace Phive\Queue;
 
 class NoItemAvailableException extends QueueException
 {
-    public function __construct(Queue $queue, $message = null, $code = null, \Exception $previous = null)
+    public function __construct(Queue $queue, $message = null, $code = 0, \Exception $previous = null)
     {
         parent::__construct($queue, $message ?: 'No items are available in the queue.', $code, $previous);
     }
